e7fa6299aa04d0b4e397c941220b872dbb997c49
JordanFisher1 <jafisher1@ualr.edu>
2022-01-26 16:22:11 +0000

    Unit 1

---
+++ 01.05 Previous and Next.py
@@ -0,0 +1,8 @@
+x = int(input("Enter Number: "))
+next = x + 1
+prev = x - 1 
+a = "The next number for the number"
+b = "is"
+c = "The previous number for the number"
+print (a + " " + str(x) + " " + b + " " + str(next) + ".") 
+print ( c + " " + str(x) + " " + b + " " + str(prev) + ".")
