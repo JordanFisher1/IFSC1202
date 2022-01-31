feee01edc938c709490fd1fd45cb520823d13a15
JordanFisher1 <jafisher1@ualr.edu>
2022-01-31 15:53:14 +0000

    Unit 1

---
+++ 02.01 Last Digit.py
@@ -0,0 +1,3 @@
+x=int(input("enter the number:"))
+lastdigit=x%10
+print("last digit: {}".format(lastdigit))
---
+++ 03.00.02 If and Else.py
@@ -0,0 +1,6 @@
+x = int(input("Enter X: "))
+if x > 0:
+    print(x)
+    print(x)
+else:
+    print(-x)
