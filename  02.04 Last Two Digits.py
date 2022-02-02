5abfb8f905a4b181773137454b466fa0579d8086
JordanFisher1 <jafisher1@ualr.edu>
2022-02-02 15:05:00 +0000

    Unit 1

---  02.04 Last Two Digits.py
+++  02.04 Last Two Digits.py
@@ -1,6 +1,6 @@
 x = int(input("Enter a number: "))
 onesplace = x%10
-tensplace = int(x/50)
-hundredsplace = 
-lasttwodigits = (str(tensplace)+ str(onesplace))
+tensplace = int(x/10)%10
+hundredsplace =  x%1
+lasttwodigits = (str(hundredsplace) + (str(tensplace)+ str(onesplace)))
 print ("Last Two digits: {}".format(lasttwodigits))
