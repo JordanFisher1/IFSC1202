d5962388641965ca8a4fd99d752f9e0436fc9e5a
JordanFisher1 <jafisher1@ualr.edu>
2022-02-02 17:15:24 +0000

    Unit 1

---
+++ 03.02 Equal Numbers.py
@@ -0,0 +1,12 @@
+x = input("Enter First Number: ")
+y = input("Enter Second Number: ")
+z = input("Enter Third Number: ")
+if x == y and x == z:
+    equal = (3)
+if x == y and x != z:
+    equal = (2)
+if x == z and x != y:
+    equal = 2
+if x != z and x != y:
+    equal = (0)
+print (equal)
