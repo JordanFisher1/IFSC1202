07ea4b82a98e8edfc09aca6c20c0c2d40190ec7a
JordanFisher1 <jafisher1@ualr.edu>
2022-02-14 15:59:44 +0000

    Unit 1

---
+++ 04.01 Product of N Numbers.py
@@ -0,0 +1,6 @@
+N = int(input("Enter N: "))
+product = 1
+for N in range(1, N+1):
+    x = (int(input("Enter Number: "))) 
+    product = product * x
+print(product)
---
+++ 04.03 Product of N Numbers.py
@@ -0,0 +1,5 @@
+N = int(input("Enter Number; "))
+product = 1
+for i in range(1,N+1):
+    product = product * i
+print(product)
